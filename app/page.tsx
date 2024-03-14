e0a46cccaab4c30edf273da13606d521d87e6cd6
CODELOCCOL <codeloccol@Abdouls-iMac.local>
2024-03-14 01:56:20 +0100

    Redo the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Login from './(login)/signIn/Login'
+import Link from 'next/link'
 
 const page = () => {
   return (
-    <div className='w-full'>
-      <Login/>
+    <div className='w-full flex justify-center items-center' style={{minHeight: "100vh"}}>
+      <Link href={'/signIn'} className='btn-primary mx-5'>Singin</Link>
+      <Link href={'/signUp'} className='btn'>SingUp</Link>
     </div>
   )
 }
