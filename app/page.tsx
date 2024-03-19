0b3fcf09bab7e1c77cbd2a183cb2bbfea43af810
CODELOCCOL <codeloccol@Abdouls-iMac.local>
2024-03-20 00:17:39 +0100

    fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
-    <div className='w-full flex justify-center items-center' style={{minHeight: "100vh"}}>
-      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'><Link href={'/signIn'} className='btn-primary mx-5'>Singin</Link></button>
-     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 rounded'> <Link href={'/signUp'} className='btn'>SingUp</Link></button>
+    <div
+      className="w-full flex justify-center items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <Link href={"/signIn"}>Singin</Link>
+      </button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 rounded">
+        {" "}
+        <Link href={"/signUp"}>SingUp</Link>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
