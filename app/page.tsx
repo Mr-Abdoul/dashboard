5253582249b4db0e443f399d20c4750b6cae3643
CODELOCCOL <codeloccol@Abdouls-iMac.local>
2024-03-20 00:12:03 +0100

    fetch

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div className='w-full flex justify-center items-center' style={{minHeight: "100vh"}}>
-      <Link href={'/signIn'} className='btn-primary mx-5'>Singin</Link>
-      <Link href={'/signUp'} className='btn'>SingUp</Link>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'><Link href={'/signIn'} className='btn-primary mx-5'>Singin</Link></button>
+     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'> <Link href={'/signUp'} className='btn'>SingUp</Link></button>
     </div>
   )
 }
